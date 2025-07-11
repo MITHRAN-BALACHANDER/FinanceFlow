750824360fce73a8358c1918f8dd7a28b697d151
Mithran Balachander <bmithran15@gmail.com>
2025-07-11 15:12:12 +0000

    add the option to close and open sidebat

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import * as React from 'react';
@@ -123,7 +124,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
       </Sidebar>
       <SidebarInset>
         <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-            <SidebarTrigger className="sm:hidden" />
+            <SidebarTrigger />
             <div className="ml-auto">
               <UserNav />
             </div>
